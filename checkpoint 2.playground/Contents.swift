a6d85ad7e6b06ddd0cb59beb34af7e2c691e4dc4
Mason-Wray <mason.wray@stu.barren.kyschools.us>
2024-08-27 08:34:53 -0500

    Delete checkpoint 2.playground directory

--- checkpoint 2.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import Cocoa
-let colors = ["green", "yellow", "blue", "blue", "orange", "green", "red"]
-let totalAmount = colors.count
-
-let uniqueItems = Set(colors)
-let uniquetotal = uniqueItems.count
-
-print("total = \(totalAmount) ")
-print("unique total = \(uniquetotal) ")
